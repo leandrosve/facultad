ffa00a6d367c5bb7ab3a6441c970ca8534e137ad
leandro <leandrosvetlich@gmail.com>
2025-04-01 18:27:12 -0300

    Hacer que escriba los resultados en un archivo

--- sistemas_paralelos/entrega_1/realizar_pruebas.sh
+++ sistemas_paralelos/entrega_1/realizar_pruebas.sh
@@ -3,6 +3,10 @@ trap "echo 'Terminando ejecución...'; pkill -P $$; exit 1" SIGINT
 gcc -o ./compilados/ejercicio2_v1_optimo ejercicio2_v1_optimo.c -O3
 gcc -o ./compilados/ejercicio2_v2_optimo ejercicio2_v2_optimo.c -O3
 
+# Archivo de resultados
+RESULT_FILE="./compilados/resultados.txt"
+echo "Resultados de ejecución" > "$RESULT_FILE"
+
 # Valores de N
 N_VALUES=(512 1024 2048 4096)
 # Valores de BLOCK_SIZE
@@ -11,7 +15,7 @@ BLOCK_SIZES=(8 16 32 64 128 256 512)
 # Ejecutar la versión 1
 for N in "${N_VALUES[@]}"; do
     echo "Ejecutando ejercicio2_v1_optimo con N=$N"
-    ./compilados/ejercicio2_v1_optimo $N
+    ./compilados/ejercicio2_v1_optimo $N | tee -a "$RESULT_FILE"
     echo "--------------------------------------"
 done
 
@@ -19,7 +23,7 @@ done
 for N in "${N_VALUES[@]}"; do
     for BLOCK_SIZE in "${BLOCK_SIZES[@]}"; do
         echo "Ejecutando ejercicio2_v2_optimo con N=$N y BLOCK_SIZE=$BLOCK_SIZE"
-        ./compilados/ejercicio2_v2_optimo $N $BLOCK_SIZE
+        ./compilados/ejercicio2_v2_optimo $N $BLOCK_SIZE | tee -a "$RESULT_FILE"
         echo "--------------------------------------"
     done
 done
